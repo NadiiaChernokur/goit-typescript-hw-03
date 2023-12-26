94f97b8faae6384eba68da01f3a8b1cef45d242e
NadiiaChernokur <chiernokur5@gmail.com>
2023-12-26 21:15:51 +0100

    treee

--- src/4.ts
+++ src/4.ts
@@ -39,7 +39,7 @@ class MyHouse extends House {
 }
 const key = new Key();
 
-const house = new MyHouse(true, key);
+const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
